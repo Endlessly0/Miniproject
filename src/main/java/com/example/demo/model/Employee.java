572e44470e1a177f1e6960beccf056e3b308b122
Endlessly0 <6310210279@psu.ac.th>
2023-07-12 12:06:59 +0700

    lab2 getAll, Create employee

---
+++ src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,46 @@
+package com.example.demo.model;
+
+public class Employee {
+	
+	private Integer employeeID;
+	private String firstName;
+	private String lastName;
+	private String salary;
+	
+	
+	public Employee(Integer employeeID, String firstName, String lastName, String salary) {
+		super();
+		this.employeeID = employeeID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+		
+	}
+	public Integer getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(Integer employeeID) {
+		this.employeeID = employeeID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+}
