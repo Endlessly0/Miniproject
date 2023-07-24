9690913997c7a9e42cccb55b92c608d5e16975bc
QILLUAZ <6310210279@psu.ac.th>
2023-07-24 19:19:36 +0700

    Lab2 CRUD Employee and Product

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,8 +3,11 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +34,43 @@ public class EmployeeController {
 		data.add(body);
 		return body;
 		
+	}
+	
+	@GetMapping("/employee/{employeeId}")
+	public Employee getEmployeeDetail (@PathVariable Integer employeeId) {
+	
+		for (int i = 0; i < data.size(); i++) {
+			if (employeeId == data.get(i).getEmployeeID()) {
+				return data.get(i);
+			}
+		}
+		return null;
+	}
+	
+	@PutMapping("/employee/{employeeId}")
+	public Employee updateEmployee(@PathVariable Integer employeeId, @RequestBody Employee body ) {
+		
+		for (int i = 0; i < data.size(); i++) {
+			if (employeeId == data.get(i).getEmployeeID()) {
+				data.get(i).setFirstName(body.getFirstName());
+				data.get(i).setLastName(body.getLastName());
+				data.get(i).setSalary(body.getSalary());
+				return data.get(i);
+			}
+		}
+		
+		return null;
+	}
+	
+	@DeleteMapping("/employee/{employeeId}")
+	public String deleteEmployee(@PathVariable Integer employeeId){
+		for (int i = 0; i < data.size(); i++) {
+			if (employeeId == data.get(i).getEmployeeID()) {
+				data.remove(i);
+				return "delete sucsess";
+			}
+		}
+		
+		return "employee not found";
 	}
 }
